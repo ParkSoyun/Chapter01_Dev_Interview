dd75d694d42be960e6dd81d1c5b64643b30106ba
dongil618 <rlaehddlf618@gmail.com>
2022-06-20 23:48:09 +0900

    CREATE app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# load .env
+load_dotenv()
+
+# Connect DB
+mongo_url = os.environ.get('MONGO_URL')
+client = MongoClient(mongo_url)
+db = client.dbsparta
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
