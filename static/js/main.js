c8aa3eb65c357934cc71de96e47bdaae7fa99c93
dongil618 <rlaehddlf618@gmail.com>
2022-06-22 23:46:28 +0900

    UPDATE main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,14 +2,14 @@ function check_user() {
     let password = $('#inputPassword').val()
     $.ajax({
         type: "POST",
-        url: "/check-userinfo",
+        url: "/checkuserinfo",
         data: {password_give: password},
         success: function (response) {
             let msg = response["msg"];
             let user = response["user"];
             if (msg == "success") {
                 alert("확인완료")
-                get_userinfo(user);
+                window.location.replace("/userinfo")
             } else {
                 alert("비밀번호가 틀렸습니다.")
                 $('#inputPassword').val("");
@@ -18,13 +18,58 @@ function check_user() {
     });
 }
 
-function get_userinfo(user){
+function edit_userinfo(){
+    let username = $("#inputUserName").val()
+    let email = $("#inputEmail").val()
+    let password = $("#inputNewPassword").val()
+    let password2 = $("#inputCheckNewPassword").val()
+
+    if (username == "") {
+        $("#help-email").text("이름을 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#inputUserName").focus()
+        return;
+    }
+
+    if(!email.includes('@')) {
+        $("#help-email").text("email 형식으로 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#inputEmail").focus()
+        return;
+    }
+
+    if (password == "") {
+        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#inputPassword").focus()
+        return;
+    } else if (!is_password(password)) {
+        $("#help-password").text("비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자").removeClass("is-safe").addClass("is-danger")
+        $("#inputPassword").focus()
+        return;
+    } else {
+        $("#help-password").text("사용할 수 있는 비밀번호입니다.").removeClass("is-danger").addClass("is-success")
+    }
+    if (password2 == "") {
+        $("#help-password").text("비밀번호를 입력해주세요.").removeClass("is-safe").addClass("is-danger")
+        $("#inputCheckPassword").focus()
+        return;
+    } else if (password2 != password) {
+        $("#help-password").text("비밀번호가 일치하지 않습니다.").removeClass("is-safe").addClass("is-danger")
+        $("#inputCheckPassword").focus()
+        return;
+    } else {
+        $("#help-password").text("비밀번호가 일치합니다.").removeClass("is-danger").addClass("is-success")
+    }
+
     $.ajax({
-        type: "POST",
+        type: "PUT",
         url: "/userinfo",
-        data: {name_give: user[0]["name"], email_give: user[0]["email"]},
+        data: {
+            name_give: username,
+            email_give : email,
+            password_give: password
+        },
         success: function (response) {
-            console.log(response);
+            alert("회원정보 수정 완료!")
+            window.location.replace("/question")
         }
     });
 }
@@ -112,14 +157,18 @@ function sign_in() {
             password_give: password
         },
         success: function (response) {
-            console.log(response['result'])
             if (response['result'] == 'success') {
-                $.cookie('mytoken', response['token'], {path: '/question'});
-                console.log($.cookie('mytoken', response['token'], {path: '/question'}))
-                //window.location.replace("/question")
+                $.cookie('mytoken', response['token'], {path: '/'});
+                window.location.replace("/question")
             } else {
                 alert(response['msg'])
             }
         }
     });
+}
+
+function sign_out() {
+    $.removeCookie('mytoken', {path: '/'});
+    alert('로그아웃!')
+    window.location.href = "/signin"
 }
