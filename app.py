5bdfceef4dbd062532987e6f2f9b3d85c8531454
dongil618 <rlaehddlf618@gmail.com>
2022-06-22 23:39:10 +0900

    UPDATE app.py - my_question 라우트 추가

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.sign_up import sign_up
 from routes.sign_in import sign_in
 from routes.my_page import my_page
 from routes.index import index
+from routes.my_question import my_question
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.register_blueprint(my_page)
 app.register_blueprint(sign_up)
 app.register_blueprint(sign_in)
 app.register_blueprint(index)
+app.register_blueprint(my_question)
 
 
 if __name__ == '__main__':
