0d40162beb41ed2881070b27841217e912c34925
dongil618 <rlaehddlf618@gmail.com>
2022-06-23 10:29:40 +0900

    답변모아보기 기능 구현

--- routes/my_question.py
+++ routes/my_question.py
@@ -23,6 +23,20 @@ def check_user_info_get():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         signin_status = True
         user = db.user.find_one({"email": payload["id"]})
-        return render_template('my-question.html', signin_status=signin_status, user_info=user)
+        solved_list = user['solved']
+        question_list = []
+        for solved in solved_list:
+            question_collection = db.question.find_one({"num": int(solved)})
+            question_num = question_collection['num']
+            question = question_collection['question']
+            answer_collection = db.answer.find_one({"question_num": int(solved), "user_email": payload["id"]})
+            if answer_collection != "None":
+                answer = answer_collection['answer']
+                like_count = answer_collection['like_count']
+            # print(question_collection)
+            # print(answer_collection)
+            question_list.append((question_num, question, answer, like_count))
+
+        return render_template('my-question.html', signin_status=signin_status, user_info=user, question_list=question_list)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return render_template("sign-in.html")
